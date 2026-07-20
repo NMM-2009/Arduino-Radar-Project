240f45b1df4db10be65d1248196f72e2a584f60f
NMM-2009 <zz84mmm85n@privaterelay.appleid.com>
2026-07-20 11:40:24 +0100

    Refactor radar.ino for improved readability and functionality

--- radar/radar.ino
+++ radar/radar.ino
@@ -1,7 +1,8 @@
+#include <LiquidCrystal.h>
 #include <SR04.h>
 #include <Servo.h>
 
-unsigned long lastBeep = 0; // Used to have delays with buzzer without delaying the rest of the system
+unsigned long lastBeep = 0; // Allows to have delays with buzzer without delaying the rest of the system
 int beepInterval;
 
 Servo myServo;
@@ -24,40 +25,60 @@ long a;
 
 int pot = A0;
 
+LiquidCrystal lcd(13, 8, A2, A3, A4, A5);
+
+int threshold;
+
+void setScreen(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("State: ");
+  if (setting == 0){
+    lcd.print("Unarmed");
+  }else{
+    lcd.print("Armed");
+  }
+  lcd.setCursor(0, 1);
+  lcd.print("Threshold: ");
+  lcd.print(threshold);
+  lcd.print("cm");
+}
+
 void measure(){
   // Check if button is pressed and switch to arm/disarm
   if (digitalRead(button) == LOW){
       setting = 1 - setting;
       delay(200);
     }
-
   // Get distance from ultrasonic sensor and check if an object is within a certain distance
   a = sr04.Distance();
-  int threshold = map(analogRead(pot), 0, 1023, 5, 60);
+  threshold = map(analogRead(pot), 0, 1023, 5, 60);
+  setScreen();
   if (setting == 1){
     if (a <= threshold){
       beepInterval = map(a, 0, threshold, 1, 500);
       alarm();
     }else{
-      digitalWrite(GREEN, 125);
-      digitalWrite(RED, 125);
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(RED, HIGH);
       digitalWrite(BLUE, LOW);
       digitalWrite(buzzer, LOW);
       buzzerState = 0;
     }
 
   }else{
-    digitalWrite(GREEN, 125);
+    digitalWrite(GREEN, HIGH);
     digitalWrite(RED, LOW);
     digitalWrite(BLUE, LOW);
     digitalWrite(buzzer, LOW);
     buzzerState = 0;
   }
 }
+
 // Turn LED to red and have buzzer beep increasingly quickly as object gets closer
 void alarm(){
   digitalWrite(GREEN, LOW);
-  digitalWrite(RED, 125);
+  digitalWrite(RED, HIGH);
   digitalWrite(BLUE, LOW);
 
   unsigned long now = millis();
@@ -81,6 +102,8 @@ void setup() {
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  lcd.begin(16, 2);
+  setScreen();
 }
 
 void loop() {
@@ -95,6 +118,6 @@ void loop() {
   for (pos = 180; pos >= 0; pos --){
     myServo.write(pos);
     measure();
-
+    delay(10);
   }
 }

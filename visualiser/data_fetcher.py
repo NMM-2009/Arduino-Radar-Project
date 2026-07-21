3498947ab0f1c978d4882c51e605a0b68402e9a2
NMM-2009 <zz84mmm85n@privaterelay.appleid.com>
2026-07-21 14:47:39 +0100

    Refactor serial reading into getData function
    Refactored serial reading into getData function for better modularity and error handling.

--- visualiser/data_fetcher.py
+++ visualiser/data_fetcher.py
@@ -4,16 +4,9 @@ import time
 arduino = serial.Serial(port = "/dev/cu.usbmodem14301", baudrate = 9600, timeout = 0.1)
 time.sleep(2)
 
-# Continuously read from serial on arduino until a keyboard interruption (Ctrl + C) is used then it stops
-try:
-    while True:
-        raw = arduino.readline() # b'16\n'
-        decoded = raw.decode('utf-8') # '16\'
-        cleaned = decoded.strip() # '16'
-        if cleaned == "":
-            continue
-        print(int(cleaned))
-except KeyboardInterrupt:
-    print("Stopping")
-finally:
-    arduino.close()
+def getData():
+    line = arduino.readline().decode('utf-8').strip()
+    if line == "":
+        return None
+    angleStr, distanceStr, detectedStr = line.split(",")
+    return int(angleStr), int(distanceStr), int(detectedStr)
